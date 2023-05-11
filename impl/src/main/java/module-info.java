b8e74a027c68c04cc9466242baadcf37e68f6d26
Lukas Jungmann <lukas.jungmann@oracle.com>
2023-05-11 14:34:54 +0200

    update build plugins
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- impl/src/main/java/module-info.java
+++ impl/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -14,6 +14,9 @@
  * SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
  */
 
+/**
+ * Jakarta JSON API with Eclipse Parsson implementation.
+ */
 module org.eclipse.parsson {
     requires transitive jakarta.json;
     exports org.eclipse.parsson.api;
--- rest/src/main/java/org/eclipse/parsson/media/JsonValueBodyWriter.java
+++ rest/src/main/java/org/eclipse/parsson/media/JsonValueBodyWriter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2012, 2022 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2012, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -55,6 +55,11 @@ public class JsonValueBodyWriter implements MessageBodyWriter<JsonValue> {
 
     private final Configuration config;
 
+    /**
+     * Constructor.
+     *
+     * @param config configuration
+     */
     public JsonValueBodyWriter(@Context Configuration config) {
         this.config = config;
     }
