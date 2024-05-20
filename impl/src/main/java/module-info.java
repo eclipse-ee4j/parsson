74e689ae284fce953ab44725e2bfeaf81944a427
Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>
2024-05-20 10:18:08 +0200

    Enable jakarta.json.provider system property to work in JPMS apps. Provide explicit API to create JsonProvider instance #129
    Signed-off-by: Jorge Bescos Gascon <jorge.bescos.gascon@oracle.com>

--- impl/src/main/java/module-info.java
+++ impl/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2016, 2023 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2016, 2024 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -20,5 +20,6 @@
 module org.eclipse.parsson {
     requires transitive jakarta.json;
     exports org.eclipse.parsson.api;
+    opens org.eclipse.parsson to jakarta.json;
     provides jakarta.json.spi.JsonProvider with org.eclipse.parsson.JsonProviderImpl;
 }
